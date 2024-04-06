4cb95eea6cea0e6ac9571b8d24a45eedf13724a1
Marcela Rodriguez <marcelaaprodriguez@gmail.com>
2024-04-06 13:47:04 -0300

    People System

---
+++ People_System/src/tech/marcelarodriguez/composicoes/Profissao.java
@@ -0,0 +1,11 @@
+package tech.marcelarodriguez.composicoes;
+
+public enum Profissao { //Classe criada para receber os valores;
+    ANALISTA_DE_SISTEMAS,
+    CORRETOR_DE_IMOVEIS,
+    EMPRESARIO,
+    ESTAGIARIO,
+    PROFESSOR,
+    QA;
+
+    }
---
+++ People_System/src/tech/marcelarodriguez/composicoes/Telefone.java
@@ -0,0 +1,22 @@
+package tech.marcelarodriguez.composicoes;
+
+public class Telefone {
+    private String ddd;
+    private String numTel;
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumTel() {
+        return numTel;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
+    }
+}
